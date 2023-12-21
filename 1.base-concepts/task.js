2210826d9fb1c14d9b28864201c31ae9dce0a245
Bolshova <bolshova.zoya@gmail.com>
2023-12-22 00:23:35 +0300

    Реализовано первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,21 @@
 "use strict"
 function solveEquation(a, b, c) {
+  let d = Math.pow(b,2)-4*a*c;
   let arr = [];
-  
+  if (d===0) {
+    arr.push(-b / (2 * a))
+  } else if (d > 0) {
+    arr.push((-b + Math.sqrt(d) )/(2*a))
+    arr.push((-b - Math.sqrt(d) )/(2*a))
+  }
   return arr;
 }
+solveEquation ()
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let loanBody = amount - contribution;
+  let correctPercent = percent / 100 / 12;
+  let payment = loanBody * (correctPercent + (correctPercent / (((1 + correctPercent) ** countMonths) - 1)));
+  let totalAmount = payment * countMonths;
+  return +totalAmount.toFixed(2)
 }
